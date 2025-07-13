174f2d5f206138f10f53f227969dd5001035a91c
DestinyDriver <destinydriverx@gmail.com>
2025-07-13 12:58:54 +0530

    Added Favicon

--- app/page.js
+++ app/page.js
@@ -52,7 +52,7 @@ const Page = () => {
             <div className="flex w-full max-w-sm flex-col gap-6">
               <div className="flex items-center gap-2 self-center font-medium">
                 <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                  <Rabbit className="size-4" />
+                  <Rabbit className="size-4 text-neutral-900" />
                 </div>
                 Filo Inc.
               </div>
@@ -71,7 +71,7 @@ const Page = () => {
         {/* Logo */}
         <div className="flex items-center space-x-2 ">
           
-          <div className='h-9 w-9 bg-gray-100 flex items-center justify-center rounded-md'><Rabbit className="text-gray-900 h-7 w-7 " /></div>
+          <div className='h-9 w-9 bg-gray-100 flex items-center justify-center rounded-md'><Rabbit className="text-neutral-900 h-7 w-7 " /></div>
           
           <p className={`text-3xl  ${dancingScript.className} dark:text-gray-100 `}>Filo</p>
         </div>
